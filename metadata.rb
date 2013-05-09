341f10df5e1efdf6a2d76d4bc86d574b9a031ea6
Jeppe Nejsum Madsen <jeppe@ingolfs.dk>
2013-05-09 13:08:58 +0200

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             "sql_server"
 maintainer       "Opscode, Inc."
 maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
-description      "Installs/Configures Microsoft SQL Server 2008 R2/2012"
+description      "Installs/Configures Microsoft SQL Server 2008 R2/2012 and stuff"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.2.6"
 supports         "windows"
