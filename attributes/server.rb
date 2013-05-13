13a7355e88458ae0e24a0f08861d156009bf88bd
Jeppe Nejsum Madsen <jeppe@ingolfs.dk>
2013-05-13 09:18:58 +0200

    Update to 2012 SP1

--- attributes/server.rb
+++ attributes/server.rb
@@ -25,17 +25,15 @@ default['sql_server']['instance_name']  = 'SQLEXPRESS'
 default['sql_server']['instance_dir']   = 'C:\Program Files\Microsoft SQL Server'
 
 if kernel['machine'] =~ /x86_64/
-    default['sql_server']['server2012']['url']          = 'http://download.microsoft.com/download/8/D/D/8DD7BDBA-CEF7-4D8E-8C16-D9F69527F909/ENU/x64/SQLEXPR_x64_ENU.exe'
-    default['sql_server']['server2012']['checksum']     = '7f5e3d40b85fba2da5093e3621435c209c4ac90d34219bab8878e93a787cf29f'
+    default['sql_server']['server2012']['url']          = 'http:////download.microsoft.com/download/5/2/9/529FEF7B-2EFB-439E-A2D1-A1533227CD69/SQLEXPR_x64_ENU.exe'
+    default['sql_server']['server2012']['checksum']     = '7fae66c782d2fa3428530a074d091b51dcd17dee52b5a031c58505b01027d10f'
     default['sql_server']['server2012']['package_name'] = 'Microsoft SQL Server 2012 (64-bit)'
 
     default['sql_server']['server']['url']          = 'http://care.dlservice.microsoft.com/dl/download/5/1/A/51A153F6-6B08-4F94-A7B2-BA1AD482BC75/SQLEXPR_x64_ENU.exe'
     default['sql_server']['server']['checksum']     = '6840255cf493927a3f5e1d7f865b8409ed89133e3657a609da229bab4005b613'
     default['sql_server']['server']['package_name'] = 'Microsoft SQL Server 2008 R2 (64-bit)'
 else
-
   default['sql_server']['server']['url']          = 'http://care.dlservice.microsoft.com/dl/download/5/1/A/51A153F6-6B08-4F94-A7B2-BA1AD482BC75/SQLEXPR32_x86_ENU.exe'
   default['sql_server']['server']['checksum']     = '24f75df802a406cf32e854a60b0c340a50865fb310c0f74c7cecc918cff6791c'
   default['sql_server']['server']['package_name'] = 'Microsoft SQL Server 2008 R2 (32-bit)'
-
 end
